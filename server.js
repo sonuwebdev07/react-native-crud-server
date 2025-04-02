5b93ea26ae98a2fc4f89e5aa9efc8e1bb9d914b2
sonugautam <sonuwebdev07@gmail.com>
2025-04-02 15:23:21 +0530

    add home route

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ app.use(morgan('dev'));
 app.use('/api/v1/auth', require('./routes/userRoutes'));
 app.use('/api/v1/post', require('./routes/postRoutes'));
 
+//Home
+app.get('/', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Node Server Running"
+    })
+})
+
 //PORT
 const PORT = process.env.PORT || 8080;
 
